a355c79a84121bb51f580daf8e215b79edab1e13
Mikaël Armonia <marmonia@sqli.com>
2019-01-25 14:25:10 +0100

    fix: Temporary fix for pixelated images

--- android/src/main/java/com/dylanvann/fastimage/FastImageViewWithUrl.java
+++ android/src/main/java/com/dylanvann/fastimage/FastImageViewWithUrl.java
@@ -4,11 +4,17 @@ import android.content.Context;
 import android.widget.ImageView;
 
 import com.bumptech.glide.load.model.GlideUrl;
+import com.facebook.react.bridge.ReadableMap;
 
 class FastImageViewWithUrl extends ImageView {
+    public static final ScaleType defaultScaleType = ScaleType.CENTER_CROP;
     public GlideUrl glideUrl;
 
     public FastImageViewWithUrl(Context context) {
         super(context);
+
+        // Temporary fix for pixelated images
+        // TODO: find a way to call setResizeMode before setSrc in FastImageViewManager
+        setScaleType(defaultScaleType);
     }
 }
